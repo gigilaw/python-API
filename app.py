df844c991d3fa5d6a729425bc867b028373a9f58
Gigi Law <gigi.ht.law.pccw@gmail.com>
2021-09-18 01:47:23 +0800

    feat(route): get all doctors

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from faker import Faker
 from datetime import datetime
+from flask_marshmallow import Marshmallow
+from marshmallow import fields
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import Schema, fields
 
 app = Flask(__name__)
 
@@ -12,24 +16,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 fake = Faker()
+ma = Marshmallow(app)
 
-doctors_clinics = db.Table('doctors_clinics',
+
+doctor_language = db.Table('doctor_language',
                            db.Column('doctor_id', db.Integer, db.ForeignKey(
                                'doctor.id'), primary_key=True),
-                           db.Column('clinic_id', db.Integer, db.ForeignKey(
-                               'clinic.id'), primary_key=True),
+                           db.Column('language_id', db.Integer, db.ForeignKey(
+                               'language.id'), primary_key=True),
                            db.Column('created_at', db.DateTime,
                                      default=datetime.now())
                            )
-
-doctors_languages = db.Table('doctors_languages',
-                             db.Column('doctor_id', db.Integer, db.ForeignKey(
-                                 'doctor.id'), primary_key=True),
-                             db.Column('language_id', db.Integer, db.ForeignKey(
-                                 'language.id'), primary_key=True),
-                             db.Column('created_at', db.DateTime,
-                                       default=datetime.now())
-                             )
 
 
 class Doctor(db.Model):
@@ -37,56 +34,38 @@ class Doctor(db.Model):
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     gender = db.Column(db.String, nullable=False)
-    price = db.Column(db.String, nullable=False)
-    include_medicine = db.Column(db.Boolean, default=True, nullable=False)
-    medicine_days = db.Column(db.Integer, nullable=True)
-    hours = db.Column(db.String, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'),
                             nullable=False)
+    clinic_id = db.Column(db.Integer, db.ForeignKey('clinic.id'),
+                          nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now())
 
-    clinics = db.relationship('Clinic', secondary=doctors_clinics)
-    languages = db.relationship('Language', secondary=doctors_languages)
+    languages = db.relationship(
+        'Language', secondary=doctor_language, lazy="joined")
+    clinic = db.relationship('Clinic', backref='doctor')
 
-    def __init__(self, first_name, last_name, gender, price, include_medicine, medicine_days, hours, category_id):
+    def __init__(self, first_name, last_name, gender, category_id, clinic_id):
         self.first_name = first_name
         self.last_name = last_name
         self.gender = gender
-        self.price = price
-        self.include_medicine = include_medicine
-        self.medicine_days = medicine_days
-        self.hours = hours
         self.category_id = category_id
+        self.clinic_id = clinic_id
 
     @classmethod
     def seed(cls, fake):
-        mock_hours = [
-            "Monday: 9am-3pm, Tuesday: 9am-3pm, Wednesday: 9am-3pm, Thursday: 9am-3pm",
-            "Monday: 9am-8pm, Friday: 9am-8pm, Saturday: closed, Sunday: closed, Public Holiday: closed"
-        ]
         doctor = Doctor(
             first_name=fake.first_name(),
             last_name=fake.last_name(),
             gender=fake.random_element(elements=("F", "M")),
-            price=fake.random_int(300, 1000, 10),
-            include_medicine=fake.boolean(),
-            medicine_days=None,
-            hours=fake.random_element(elements=mock_hours),
-            category_id=fake.random_int(1, 3)
+            category_id=fake.random_int(1, 3),
+            clinic_id=fake.random_int(1, 5)
         )
-        if doctor.include_medicine:
-            doctor.medicine_days = fake.random_int(1, 7)
-
         save(doctor)
-        doctor.add_relationship()
+        doctor.add_mock_relationship()
 
-    def add_relationship(self):
-        clinic = Clinic.query.get(fake.random_int(1, 5))
-        self.clinics.append(clinic)
-
+    def add_mock_relationship(self):
         language = Language.query.get(fake.random_int(1, 3))
         self.languages.append(language)
-
         db.session.commit()
 
 
@@ -116,26 +95,45 @@ class Clinic(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     address = db.Column(db.String, nullable=False)
-    district_id = db.Column(db.Integer, db.ForeignKey('district.id'),
-                            nullable=False)
+    district_id = db.Column(db.Integer, db.ForeignKey(
+        'district.id'), nullable=False)
     contact = db.Column(db.String, nullable=False)
+    hours = db.Column(db.String, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    include_medications = db.Column(db.Boolean, nullable=False)
+    medication_days = db.Column(db.Integer, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.now())
 
-    def __init__(self, name, address, district_id, contact):
+    def __init__(self, name, address, district_id, contact, hours, price, include_medications, medication_days):
         self.name = name
         self.address = address
         self.district_id = district_id
         self.contact = contact
+        self.hours = hours
+        self.price = price
+        self.include_medications = include_medications
+        self.medication_days = medication_days
 
     @classmethod
     def seed(cls, fake):
+        mock_hours = [
+            "Monday: 9am-3pm, Tuesday: 9am-3pm, Wednesday: 9am-3pm, Thursday: 9am-3pm",
+            "Monday: 9am-8pm, Friday: 9am-8pm, Saturday: closed, Sunday: closed, Public Holiday: closed"
+        ]
+
         clinic = Clinic(
             name=fake.company() + " Clinic",
             address=fake.address(),
             district_id=fake.random_int(1, 2),
             contact=fake.phone_number(),
+            hours=fake.random_element(elements=mock_hours),
+            price=fake.random_int(100, 1000, 10),
+            include_medications=fake.boolean(),
+            medication_days=None
         )
 
+        if clinic.include_medications:
+            clinic.medication_days = fake.random_int(1, 7)
         save(clinic)
 
 
@@ -148,6 +146,37 @@ class Language(db.Model):
         self.name = name
 
 
+class DistrictSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name')
+
+
+class ClinicSchema(SQLAlchemyAutoSchema):
+    district = ma.Nested(DistrictSchema)
+
+    class Meta:
+        model = Clinic
+        exclude = ('created_at', 'id')
+
+
+class CategorySchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
+
+
+class DoctorSchema(ma.Schema):
+    clinic = ma.Nested(ClinicSchema)
+    category = ma.Nested(CategorySchema)
+
+    class Meta:
+        fields = ('category', 'clinic', 'first_name', 'last_name')
+
+
+doctor_schema = DoctorSchema()
+doctors_schema = DoctorSchema(many=True)
+
+
 def save(self):
     db.session.add(self)
     db.session.commit()
@@ -155,7 +184,17 @@ def save(self):
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'hello world'
+    return jsonify({"status": "working"})
+
+
+@app.route('/doctor', methods=['GET', 'POST'])
+def doctor():
+    if(request.method == 'GET'):
+        all_doctors = Doctor.query.all()
+        result = doctors_schema.dump(all_doctors)
+        print(doctors_schema.validate(all_doctors))
+
+    return jsonify(result)
 
 
 if __name__ == '__main__':
